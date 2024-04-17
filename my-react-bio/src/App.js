26fccd263e482c0960cfee5b50c038342364a7a1
Japchaexx <andrenhek17@gmail.com>
2024-04-16 19:09:44 -0700

    First Commit

---
+++ my-react-bio/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello mommy, I am learning React. I am so excited to learn this new technology. I am going to be a great developer. I am going to make you proud. I love you so much.
+        
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
