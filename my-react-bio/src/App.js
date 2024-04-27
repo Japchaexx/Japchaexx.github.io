9ba2e2bf697d076ee7946202d2c73294b4b5ef82
Japchaexx <andrenhek17@gmail.com>
2024-04-26 20:06:26 -0700

    Base Templated

--- my-react-bio/src/App.js
+++ my-react-bio/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I love you so much.
-        
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <div className="content">
+        <h1>App Component</h1>
+      </div>
     </div>
   );
 }
---
+++ my-react-bio/src/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Wordle</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Wordle</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default NavBar;
